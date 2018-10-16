cdba4c0d0b207d50b66416b4a2e6a102970b67b4
YorkYu <ysmintor@gmail.com>
2018-10-16 09:27:29 +0800

    relocate icon resource into different directories

--- app/src/main/java/com/yorkyu/weathervision/MainActivity.java
+++ app/src/main/java/com/yorkyu/weathervision/MainActivity.java
@@ -2,6 +2,10 @@ package com.yorkyu.weathervision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.yorkyu.weathervision.util.NetUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) { Log.d("myWeather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK!", Toast.LENGTH_LONG).show();
+        }else {
+            Log.d("myWeather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了!", Toast.LENGTH_LONG).show();
+        }
     }
 }
