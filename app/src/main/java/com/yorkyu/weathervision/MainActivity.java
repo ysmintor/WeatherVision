957d32f1e265a668119f5f294741ddd94c9fec25
YorkYu <ysmintor@gmail.com>
2018-10-19 23:47:06 +0800

    add network permission

--- app/src/main/java/com/yorkyu/weathervision/MainActivity.java
+++ app/src/main/java/com/yorkyu/weathervision/MainActivity.java
@@ -1,24 +1,171 @@
 package com.yorkyu.weathervision;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.yorkyu.weathervision.util.NetUtil;
 
-public class MainActivity extends AppCompatActivity {
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "myWeather";
+    private ImageView mUpdateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) { Log.d("myWeather", "网络OK");
-            Toast.makeText(MainActivity.this,"网络OK!", Toast.LENGTH_LONG).show();
-        }else {
-            Log.d("myWeather", "网络挂了");
-            Toast.makeText(MainActivity.this,"网络挂了!", Toast.LENGTH_LONG).show();
+
+        mUpdateBtn = findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.title_update_btn) {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            String cityCode = sharedPreferences.getString("main_city_code", "101010100");
+            Log.d("myWeahter city code", cityCode);
+
+
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Log.d("myWeather", "网络OK");
+                queryWeatherCode(cityCode);
+            } else {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(MainActivity.this, "网络挂了!", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
+
+    /**
+     * @param cityCode
+     */
+    private void queryWeatherCode(String cityCode) {
+        final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+        Log.d(TAG, address);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try {
+                    URL url = new URL(address);
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("GET");
+                    con.setConnectTimeout(8000);
+                    con.setReadTimeout(8000);
+                    InputStream in = con.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String str;
+                    while ((str = reader.readLine()) != null) {
+                        response.append(str);
+                    }
+                    String responseStr = response.toString();
+//                    Log.d(TAG, responseStr);
+                    parseXML(responseStr);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (con != null) {
+                        con.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void parseXML(String xmldata) {
+        int fengxiangCount = 0, fengliCount = 0, dateCount = 0, highCount = 0, lowCount = 0, typeCount = 0;
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d(TAG, "parseXML");
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    // 判断当前事件是滞凛文档开始事件
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    // 判断当前事件是滞曾标签元素开始事件
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "city: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("updatetime")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "updatetime: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("shidu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "shidu: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("wendu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "wendu: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("pm25")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "pm25: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("quality")) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "quality: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "fengxiang: " + xmlPullParser.getText());
+                            fengxiangCount++;
+                        } else if ((xmlPullParser.getName().equals("fengli")) && fengliCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "fengli: " + xmlPullParser.getText());
+                            fengliCount++;
+                        } else if (xmlPullParser.getName().equals("date") && dateCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "date: " + xmlPullParser.getText());
+                            dateCount++;
+                        } else if (xmlPullParser.getName().equals("high") && highCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "high: " + xmlPullParser.getText());
+                            highCount++;
+                        } else if (xmlPullParser.getName().equals("low") && lowCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "low: " + xmlPullParser.getText());
+                            lowCount++;
+                        } else if (xmlPullParser.getName().equals("type") && typeCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d(TAG, "type: " + xmlPullParser.getText());
+                            typeCount++;
+                        }
+                        break;
+                    // 判断当前事件是否为标签元素结束事件
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                // 进入下一个元素并触发相应事件
+                eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
